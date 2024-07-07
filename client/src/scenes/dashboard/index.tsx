846c367678d44865ac2f1dbeba4add863d717bdf
Leandro Mangubat <leandromangubat@hotmail.com>
2024-07-07 19:45:29 -0400

    Added model and seeded Data

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,8 +1,11 @@
-import { Box, useTheme } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
+import Row1 from "./Row1";
+import Row2 from "./Row2";
+import Row3 from "./Row3";
 
 type Props = {};
 
-const gridTemplate = `
+const gridTemplateLargeScreens = `
     "a b c"
     "a b c"
     "a b c"
@@ -14,22 +17,65 @@ const gridTemplate = `
     "g h j"
     "g h j"
 `;
+const gridTemplateSmallScreens = `
+    "a"
+    "a"
+    "a"
+    "a"
+    "b"
+    "b"
+    "b"
+    "b"
+    "c"
+    "c"
+    "c"
+    "d"
+    "d"
+    "d"
+    "e"
+    "e"
+    "f"
+    "f"
+    "f"
+    "g"
+    "g"
+    "g"
+    "h"
+    "h"
+    "h"
+    "h"
+    "i" 
+    "i"
+    "j"
+    "j"
 
-const Dashboard = (props: Props) => {
-  const { palette } = useTheme();
+`;
+
+const Dashboard = () => {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
   return (
     <Box
       width="100%"
       height="100%"
       display="grid"
       gap="1.5rem"
-      sx={{
-        gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
-        gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
-        gridTemplateAreas: gridTemplate,
-      }}
+      sx={
+        isAboveMediumScreens
+          ? {
+              gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+              gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+              gridTemplateAreas: gridTemplateLargeScreens,
+            }
+          : {
+              gridAutoColumns: "1fr",
+              gridAutoRows: "80px",
+              gridTemplateAreas: gridTemplateSmallScreens,
+            }
+      }
     >
-      <Box gridArea="a"></Box>
+      <Row1 />
+      <Row2 />
+      <Row3 />
     </Box>
   );
 };
