e7e865cb1cd7b9a700e7f0b0e508bff25232e468
Leandro Mangubat <leandromangubat@hotmail.com>
2024-07-10 20:31:03 -0400

    Implemented Row 3 Transaction data UI

--- client/src/scenes/dashboard/Row3.tsx
+++ client/src/scenes/dashboard/Row3.tsx
@@ -1,12 +1,60 @@
+import {
+  useGetTransactionsQuery,
+  useGetProductsQuery,
+  useGetKpisQuery,
+} from "../../state/api";
 import Dashboardbox from "../../components/Dashboardbox";
 import React from "react";
+import { DataGrid, GridCellParams } from "@mui/x-data-grid";
+import BoxHeader from "../../components/BoxHeader";
+import { Box, useTheme } from "@mui/material";
 
-type Props = {};
+const Row3 = () => {
+  const { palette } = useTheme();
+  const { data: kpiData } = useGetKpisQuery();
+  const { data: productData } = useGetProductsQuery();
+  const { data: transactionData } = useGetTransactionsQuery();
 
-const Row3 = (props: Props) => {
+  const productColumns = [
+    {
+      field: "_id",
+      headerName: "id",
+      flex: 1,
+    },
+    {
+      field: "expense",
+      headerName: "Expense",
+      flex: 0.5,
+      renderCell: (params: GridCellParams) => `$${params.value}`,
+    },
+    {
+      field: "price",
+      headerName: "Price",
+      flex: 0.5,
+      renderCell: (params: GridCellParams) => `$${params.value}`,
+    },
+  ];
   return (
     <>
-      <Dashboardbox gridArea="g"></Dashboardbox>
+      <Dashboardbox gridArea="g">
+        <BoxHeader
+          title="List of Products"
+          sideText={`${productData?.length} products}`}
+        />
+        <Box
+          mt="0.5rem"
+          p="0 0.5rem"
+          height="75%"
+          sx={{
+            "& .MuiDataGrid-root": {
+              color: palette.grey[300],
+              border: "none",
+            },
+          }}
+        >
+          <DataGrid rows={productData || []} columns={productColumns} />
+        </Box>
+      </Dashboardbox>
       <Dashboardbox gridArea="h"></Dashboardbox>
       <Dashboardbox gridArea="i"></Dashboardbox>
       <Dashboardbox gridArea="j"></Dashboardbox>
--- server/index.js
+++ server/index.js
@@ -7,9 +7,11 @@ import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
 import productRoutes from "./routes/product.js";
+import transactionRoutes from "./routes/transaction.js";
 import Product from "./models/Product.js";
 import KPI from "./models/KPI.js";
-import { kpis, products } from "./data/data.js";
+import Transaction from "./models/Transaction.js";
+import { kpis, products, transactions } from "./data/data.js";
 
 // CONFIGURATIONS
 dotenv.config();
@@ -27,6 +29,7 @@ console.log("hello");
 // ROUTES
 app.use("/kpi", kpiRoutes);
 app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 9000;
@@ -40,6 +43,7 @@ mongoose
     // ADD DATA ONE TIME ONLY OR AS NEEDED
     // await mongoose.connection.db.dropDatabase();
     // KPI.insertMany(kpis);
-    //Product.insertMany(products);
+    // Product.insertMany(products);
+    // Transaction.insertMany(transactions);
   })
   .catch((error) => console.log(`${error} did not connect`));
